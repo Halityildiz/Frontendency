68da0ba80f1482aad80fcc8da948b833509cf574
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-10-07 13:08:08 +0300

    düzenleme yapıldı

---
+++ JavaScript-Udemy/1-1/script.js
@@ -0,0 +1,23 @@
+// var sum = 0;
+// for (var i = 3; i < 7; i++) {
+// sum += i;
+// if (i ===3 || sum > 10) {
+// continue;
+// }
+// console.log(i);
+// }
+
+// var players = ["Messi ", "Ronaldo ", "Hazard ", "Neymar ", "Salah "];
+// var text = "";
+// var i;  
+// for (i = 2; i < players.length; i++) {
+// text += players[i];
+// }
+// document.write(text);
+
+var i = 0;
+var sum=0;
+while (i < 3) {
+var sum = sum + i; i++;
+}
+console.log ("The total is: " + sum);
