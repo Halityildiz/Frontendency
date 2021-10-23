d664fba5f18391e040d042a73a2a6eaf0c5f1370
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-10-23 22:12:56 +0300

    proje çalışmaları eklendi

--- 10-projects-10-hours-master/todo-app/script.js
+++ 10-projects-10-hours-master/todo-app/script.js
@@ -5,6 +5,7 @@ const todosUL = document.getElementById("todos");
 const todos = JSON.parse(localStorage.getItem("todos"));
 
 if (todos) {
+    
     todos.forEach((todo) => {
         addTodo(todo);
     });
