a1ba91094395116f950a01be7d061a5458a313b8
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-11-12 03:19:33 +0300

    hamburger menu eklendi

--- 80daysof/hamburger menu-4/app.js
+++ 80daysof/hamburger menu-4/app.js
@@ -1,8 +1,8 @@
-const navToggle = document.querySelector('.nav-toggle');
+const navBar = document.querySelector('.navBar');
 const links = document.querySelector('.links');
-const socialLinks = document.querySelector('.social-icons');
+const socialIcon = document.querySelector('.social-icons');
 
-navToggle.addEventListener('click', function () {
+navBar.addEventListener('click', function () {
   links.classList.toggle('show-links');
-  socialLinks.classList.toggle('show-social');
+  socialIcon.classList.toggle('show-social');
 });
