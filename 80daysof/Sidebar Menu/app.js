f431a595dd101c111684ebddaf70ef598b048e97
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-11-18 18:10:18 +0300

    ekleme yapildi

--- 80daysof/Sidebar Menu/app.js
+++ 80daysof/Sidebar Menu/app.js
@@ -2,26 +2,19 @@ const toggleBtn = document.querySelector(".sidebar-toggle");
 const closeBtn = document.querySelector(".logo");
 const sidebar = document.querySelector(".sidebar");
 
-console.log(sidebar);
-console.log(closeBtn);
-console.log(toggleBtn);
+// console.log(sidebar);
+// console.log(closeBtn);
+// console.log(toggleBtn);
 
-toggleBtn.addEventListener("click", (e) => {
-  if (e.target.parentElement.nextElementSibling.classList.contains("sidebar")) {
-    e.target.parentElement.nextElementSibling[0].style.display = "none";
-    e.target.parentElement.nextElementSibling[1].style.display = "none";
-  }
+toggleBtn.addEventListener("click", function () {
+  // if (sidebar.classList.contains("show-sidebar")) {
+  //   sidebar.classList.remove("show-sidebar");
+  // } else {
+  //   sidebar.classList.add("show-sidebar");
+  // }
+  sidebar.classList.toggle("show-sidebar");
 });
 
-// closeBtn.addEventListener("click", remover());
-
-// function adder(e) {
-//   if (e.target.parentElement.nextElementSibling.classList.contains("sidebar")) {
-//     e.target.parentElement.nextElementSibling[0].style.display = "none";
-//     e.target.parentElement.nextElementSibling[1].style.display = "none";
-//   }
-// }
-
-// function remover(e) {
-//   e.target.parentElement.nextElementSibling.style.display = "none";
-// }
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
